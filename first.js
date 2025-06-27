7b9b945564470257f68e2d5c896a3d32169eacd3
Chetanya upadhyay <162006024+Chetanya2005@users.noreply.github.com>
2025-06-27 22:46:18 +0530

    Add files via upload
    Bsics of javascript.
    console.log("YOU CAN DO IT JAVASCRIPT");

---
+++ first.js
@@ -0,0 +1,45 @@
+let arr="str";
+console.log(arr);
+
+//const ids=20;
+//console.log(ids);
+
+let account_balance = 20;
+console.log(typeof account_balance);
+
+
+let comment = "hello dosto"
+console.log(comment);
+
+//boolean
+let statement = true;
+console.log(statement);
+
+//null
+let bal=null;
+console.log(bal);
+
+//object
+//key : value
+let obj = {
+    user_name : "ROHIT",
+    account_number : 31245678,
+    balance : 420
+}
+console.log(obj);
+
+console.log(undefined!=null);
+
+//function
+  //  let fun=function(){
+    //    console.log("Hello Code Army")
+    //fun();
+    //console.log(typeof fun);
+
+    let fun=function(){
+        console.log("hello coder army");
+        return 10;
+    }
+    console.log(fun());
+
+    console.log("vaishali");    
